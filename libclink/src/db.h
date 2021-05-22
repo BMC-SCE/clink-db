465011c7ad8325fa7bce80856c4ce939da07f6ba
BMC-SCE <83493607+BMC-SCE@users.noreply.github.com>
2021-05-22 19:43:54 +0300

    Delete libclink directory

--- libclink/src/db.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <sqlite3.h>
-
-struct clink_db {
-
-  /// handle to backing SQLite database
-  sqlite3 *db;
-};
--- libclink/src/get_environ.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-/// get a pointer to environ
-__attribute__((visibility("internal")))
-char **get_environ(void);
