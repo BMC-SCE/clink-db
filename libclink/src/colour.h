ceb0c4b9cc420d9dd197a610d7c2e4d369385a4a
BMC-SCE <83493607+BMC-SCE@users.noreply.github.com>
2021-05-22 19:59:51 +0300

    Delete libclink directory

--- libclink/src/colour.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/** convert a 6-character HTML hex colour into its ANSI code equivalent
- *
- * \param html String of at least 6 hex characters representing an HTML colour
- * \returns the closest ANSI colour code
- */
-__attribute__((visibility("internal")))
-unsigned html_colour_to_ansi(const char *html);
