eb295a009ebc6173f22597298d12931ff4ef3486
BMC-SCE <83493607+BMC-SCE@users.noreply.github.com>
2021-06-08 16:06:50 +0300

    Delete libclink directory

--- libclink/src/temp_dir.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/** create a temporary directory
- *
- * \param dir [out] Path to created directory on success
- * \returns 0 on success or an errno on failure
- */
-__attribute__((visibility("internal")))
-int temp_dir(char **dir);
